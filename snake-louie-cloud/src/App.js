21378d81409f45bef493f899391896eb356e3a24
leormston <l.e.ormston@gmail.com>
2024-02-02 15:54:03 +0000

    infra for the react app

---
+++ snake-louie-cloud/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <body style={{display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", fontFamily: "Arial, sans-serif"}}>
+        <div>
+            <h1>Snake Game</h1>
+            <hr/>
+            <p>Coming soon...</p>
+        </div>
+    </body>
+    </div>
+  );
+}
+
+export default App;
