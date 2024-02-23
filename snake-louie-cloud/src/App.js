af4b95ce8ade45e1497b2aac0e4bf73e5e15af68
Louie Ormston <l.e.ormston@gmail.com>
2024-02-23 16:32:06 +0000

    updated the styling and added navbar and icon logo

--- snake-louie-cloud/src/App.js
+++ snake-louie-cloud/src/App.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import './App.css';
-import Board from './board';
+import Board from './components/board';
+import Navbar from './components/navbar';
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
+import { useState } from 'react';
+
+var localTheme = ""
+if (localStorage.getItem("theme") == null) {
+  localStorage.setItem("theme", "dark");
+  localTheme = "dark"
+}
+else {
+  localTheme = localStorage.getItem("theme");
+}
+const darkTheme = createTheme({ palette: { mode: 'dark' } });
+const lightTheme = createTheme({ palette: { mode: 'light' } });
+
 function App() {
+
+  const [cssTheme, setCssTheme] = useState(localTheme);
   return (
     <div className="App">
       <body style={{display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", fontFamily: "Arial, sans-serif"}}>
         <div>
+          <Navbar theme={cssTheme === "light" ? lightTheme : darkTheme} cssTheme={cssTheme} setCssTheme={setCssTheme}/>
             <h1>Snake Game</h1>
             <hr/> 
             <Board/>
         </div>
-     </body>
+      </body>
     </div>
   );
 }
